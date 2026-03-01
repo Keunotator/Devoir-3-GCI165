41c2f912a00468d0339d76db196133cdf33fc9c6
Jacob C. <champagne.jacob2006@gmail.com>
2026-03-01 18:45:27 -0500

    Premier draft de la question 1

--- Question 1.py
+++ Question 1.py
@@ -1,39 +1,35 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Script Python pour resoudre le probleme 2 du devoir 1 de GBA400 E2020
-
-On a un billot de bois qui flotte dans l'eau et qui est aussi retenu par une corde
-On cherche:
-a. Le poids specifique du billot
-b. La tension dans la corde
-c. L’angle d’inclinaison du billot par rapport à l’horizontale, sachant que
-la pression hydrostatique au point A est de 24.5 kPa (jaugee)
-
-M-A Boucher, juin 2020 U. de Sherbrooke
+Transformation du script en fonction
+Jacob C. 2026-03-01
 """
 # Importer les modules necessaires
 import math
 
 
-# Definir les variables
-L_TOT_BILLOT = 11  #m
-L_SUBMERGEE_BILLOT = 8  #m
-D_BILLOT= 0.2  #m
-RHO_EAU = 998  #kg/m^3
-PRESSION = 24500 #Pa
+def billot(l_totale, l_sub, d_billot, rho_eau, pression):
+    """
+    Obsolète puisque on inscrit manuellement les valeurs
+    # Definir les variables
+    L_TOT_BILLOT = 11  #m
+    L_SUBMERGEE_BILLOT = 8  #m
+    D_BILLOT= 0.2  #m
+    RHO_EAU = 998  #kg/m^3
+    PRESSION = 24500 #Pa
+    """
 
+    # Calculs
 
+    centre_flottaison= l_sub/2  # m
+    vol_sub = l_sub*(d_billot/2)**2*math.pi
+    vol_tot = l_totale*(d_billot/2)**2*math.pi
+    fb = rho_eau*9.81*vol_sub
+    rho_billot = (centre_flottaison*fb)/((l_totale/2)*9.81*vol_tot)
+    poids_spec_billot = rho_billot*9.81
+    w = rho_billot*9.81*vol_tot
+    tension_corde= fb-w
+    h = pression/(rho_eau*9.81)
+    angle = (180/math.pi)*math.asin(h/l_sub)
 
-# Calculs
-
-CENTRE_FLOTTAISON = L_SUBMERGEE_BILLOT/2 #m
-VOL_SUB = L_SUBMERGEE_BILLOT*(D_BILLOT/2)**2*math.pi
-VOL_TOT = L_TOT_BILLOT*(D_BILLOT/2)**2*math.pi
-FB = RHO_EAU*9.81*VOL_SUB
-RHO_BILLOT = (CENTRE_FLOTTAISON*FB)/((L_TOT_BILLOT/2)*9.81*VOL_TOT)
-POIDS_SPEC_BILLOT = RHO_BILLOT*9.81
-W = RHO_BILLOT*9.81*VOL_TOT
-TENSION_CORDE = FB-W
-H = PRESSION/(RHO_EAU*9.81)
-angle = (180/math.pi)*math.asin(H/L_SUBMERGEE_BILLOT)
+    return poids_spec_billot, tension_corde, angle
